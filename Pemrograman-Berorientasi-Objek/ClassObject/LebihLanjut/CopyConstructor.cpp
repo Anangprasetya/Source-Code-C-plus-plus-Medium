b720b61505fd28fcdfb61cca7b67d8188eeea19f
Anang <tyoandini@gmail.com>
2021-05-19 06:56:26 +0700

    materi lanjutan class object - copy constructor

---
+++ Pemrograman-Berorientasi-Objek/ClassObject/LebihLanjut/CopyConstructor.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Tali {
+	public :
+		int ambilPanjang();
+		Tali(int p);			// Constructor
+
+		Tali(const Tali & x);	// Copy Constructor
+
+		~Tali();				// Destructor
+
+	private :
+		int *panjang;
+};
+
+int Tali:: ambilPanjang(){
+	return *panjang;
+}
+
+Tali:: Tali(int p){
+	cout << "Constructor utama ! \n";
+
+	// Mengalokasikan memori dari pointer
+	panjang = new int;
+	*panjang = p;
+}
+
+Tali:: Tali(const Tali & x){
+	cout << "Constructor hasil copy ! \n";
+
+	panjang = new int;
+	*panjang = *x.panjang;
+}
+
+Tali:: ~Tali(){
+	cout << "Destructor \n";
+	delete panjang;
+}
+
+
+void jalankan(Tali x){
+	cout << "Panjang tali rafia : ";
+	cout << x.ambilPanjang() << endl;
+}
+
+int main(){
+	Tali rafia(20.0);
+
+	jalankan(rafia);
+
+	return 0;
+}
+
+
+/* 
+==================================================================
+===================== ANANG NUR PRASETYA     =====================
+===================== anangnr.pras@gmail.com =====================
+==================================================================
+*/
